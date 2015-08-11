0970364d9a5e69019b926180279db4c5927fed6f
Rene Olivo <minet.ws@gmail.com>
2015-08-11 18:18:06 -0400

    add electron configuration

---
+++ main.js
@@ -0,0 +1,27 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var globalWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  globalWindow = new BrowserWindow({
+    'width': 800,
+    'height': 600,
+    'web-preferences': {
+        'web-security': false,
+        'webgl': false
+    }
+  });
+
+  globalWindow.loadUrl('file://index.html');
+
+  globalWindow.on('closed', function() {
+    globalWindow = null;
+  });
+});
